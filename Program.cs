e5390e6c6affa474b5d550e280c62d3df1a5d87b
Arnaud Bagbila <arbagb@outlook.com>
2021-08-30 21:22:12 +0100

    color generated

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Backfill
 {
    class Program
    {
+
+      private const int COLORS_SHEET_COLUMNS_NUMBER = 1;
+      private const string COLOR_TEMPLATE = @".\TemplateScripts\color.sql";
+      private const string COLOR_OUTPUT_SCRIPT = @".\color";
+
       static void Main(string[] args)
       {
          string userName;
@@ -59,13 +69,95 @@ namespace Backfill
 
          if (backfillOption == "color")
          {
-            Console.WriteLine("You are about to generate the sql scripts to import colors");
+            GenerateColors(path);
          }
+
+         Console.WriteLine("Done");
+         Console.ReadKey();
       }
+
+      #region Internals
 
       private static void PrintUserName(string userName)
       {
          Console.WriteLine($"{userName} you can carry on the backfilling");
       }
+
+      private static void GenerateColors(string path)
+      {
+         var dataTables = GetDataTables(path);
+
+         var colorTable =
+            dataTables.FirstOrDefault(d => string.Equals(d.TableName, "colors", StringComparison.OrdinalIgnoreCase));
+
+         ValidateTable(colorTable, COLORS_SHEET_COLUMNS_NUMBER, "colors");
+
+         var colorBuiltValues = new List<string>();
+         var colorUId = string.Empty;
+
+         colorTable.Rows.RemoveAt(0);
+         foreach (DataRow row in colorTable.Rows)
+         {
+            colorUId = GetMandatoryValue(row, 1, "colors", "ColorUId", r => colorTable.Rows.IndexOf(r));
+            if (!Guid.TryParse(colorUId, out _))
+            {
+               throw new Exception(
+                  $"ColorUId must be a guid in the colors sheet at the row {colorTable.Rows.IndexOf(row) + 2}");
+            }
+
+            var name = GetMandatoryValue(row, 0, "colors", "Name", r => colorTable.Rows.IndexOf(r));
+
+            colorBuiltValues.Add($"  ('{colorUId}', '{name}')");
+         }
+
+         var colorScript = File.ReadAllText(COLOR_TEMPLATE);
+
+         var colorScriptUpdated = colorScript.Replace("{colorValues}", string.Join($",{Environment.NewLine}", colorBuiltValues));
+
+         File.WriteAllText($"{COLOR_OUTPUT_SCRIPT}.{DateTime.Now:HHmmddMMyyyy}.sql", colorScriptUpdated);
+
+      }
+
+      private static IEnumerable<DataTable> GetDataTables(string path)
+      {
+
+         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+         using var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+         using var reader = ExcelReaderFactory.CreateReader(stream);
+         return from table in reader.AsDataSet().Tables.OfType<DataTable>() select table;
+      }
+
+      private static void ValidateTable(DataTable table, int tableColumnsNumber, string tableName)
+      {
+         if (table == null)
+         {
+            throw new Exception($"There is no {tableName} sheet in the file.");
+         }
+
+         if (table.Columns.Count < tableColumnsNumber)
+         {
+            throw new Exception($"Missing columns in the {tableName} sheet.");
+         }
+
+         if (table.Rows.Count < 2)
+         {
+            throw new Exception($"There are no rows in {tableName} sheet.");
+         }
+      }
+
+      private static string GetMandatoryValue(DataRow row, int colIndex, string tableName, string colName, Func<DataRow, int> rowIndexGetter)
+      {
+         var value = row.ItemArray[colIndex]?.ToString();
+         if (string.IsNullOrWhiteSpace(value) || string.Equals(value, "null", StringComparison.OrdinalIgnoreCase))
+         {
+            throw new Exception($"{colName} is null or empty in '{tableName}' sheet at row {rowIndexGetter(row) + 2}");
+         }
+
+         return value;
+      }
+
+
+      #endregion
+
    }
 }
---
+++ TemplateScripts/color.sql
@@ -0,0 +1,41 @@
+﻿/*
+    Colors
+*/
+
+----Load Colors
+
+IF OBJECT_ID('TempDb..#backfillColor', 'U') IS NOT NULL
+  DROP TABLE #backfillColor
+;
+SELECT
+   [UId],
+   [Name]
+INTO
+ #backfillColor
+FROM
+ dbo.[Color]
+WHERE
+ 1 = 2
+;
+INSERT INTO #backfillColor
+VALUES
+    {colorValues}
+
+MERGE[dbo].[Color] AS TARGET
+USING #backfillColor AS SOURCE
+ON TARGET.[Name] = SOURCE.[Name]
+WHEN MATCHED THEN
+   UPDATE SET
+       TARGET.[UId] = SOURCE.[UId]
+
+WHEN NOT MATCHED BY TARGET THEN
+  INSERT (
+      [UId],
+      [Name]
+  ) VALUES (
+      SOURCE.[UId],
+      SOURCE.[Name]
+  )
+;
+
+GO
